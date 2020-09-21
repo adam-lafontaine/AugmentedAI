215df972b8a11e9109e74ead2c21969bde254cfb
adam-lafontaine <adam.lafontaine@gmail.com>
2020-09-21 06:56:28 -0400

    empty tests

--- DataAdaptor/test/data_adaptor_test.cpp
+++ DataAdaptor/test/data_adaptor_test.cpp
@@ -1,6 +1,48 @@
 #include "../src/data_adaptor.hpp"
 
+#include <string>
+#include <iostream>
+
+bool file_to_data_test();
+bool files_to_data_test();
+bool convert_and_save_test();
+bool converted_to_data_test();
+
+
 int main()
 {
+	const auto run_test = [&](const auto& test) { std::cout << (test() ? "Pass" : "Fail") << '\n'; };
+
+	run_test(file_to_data_test);
+	run_test(files_to_data_test);
+	run_test(convert_and_save_test);
+	run_test(converted_to_data_test);
+
 
 }
+
+
+bool file_to_data_test()
+{
+	return false;
+}
+
+
+bool files_to_data_test()
+{
+	return false;
+}
+
+
+bool convert_and_save_test()
+{
+	return false;
+}
+
+
+bool converted_to_data_test()
+{
+	return false;
+}
+
+
