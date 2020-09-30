b02e794f954e75346af19cafa1531777520ba64e
adam-lafontaine <adam.lafontaine@gmail.com>
2020-09-30 06:22:02 -0400

    fix root file path

--- ImageFactory/src/source_main.cpp
+++ ImageFactory/src/source_main.cpp
@@ -9,10 +9,10 @@ constexpr auto pass_dir = "D:\\test_images\\src_pass";
 
 #else
 
-constexpr auto alpha_path = "~/projects/test_images/alphabet/alphabet.png";
-constexpr auto border_path = "~/projects/test_images/alphabet/border.png";
-constexpr auto fail_dir = "~/projects/test_images/src_fail";
-constexpr auto pass_dir = "~/projects/test_images/src_pass";
+constexpr auto alpha_path = "/home/adam/projects/test_images/alphabet/alphabet.png";
+constexpr auto border_path = "/home/adam/projects/test_images/alphabet/border.png";
+constexpr auto fail_dir = "/home/adam/projects/test_images/src_fail";
+constexpr auto pass_dir = "/home/adam/projects/test_images/src_pass";
 
 #endif
 
