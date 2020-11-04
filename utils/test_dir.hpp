23c7ad91a2184cd73e8f2e88f1226525849ad374
adam-lafontaine <adam.lafontaine@gmail.com>
2020-11-04 05:40:07 -0500

    text alignment

--- utils/test_dir.hpp
+++ utils/test_dir.hpp
@@ -38,14 +38,14 @@ public:
 	TestDirConfig()
 	{
 		m_keys = cr::key_list_t(static_cast<size_t>(TestDir::Count));
-		m_keys[to_index(TestDir::PROJECT_ROOT)] =   "PROJECT_ROOT";
-		m_keys[to_index(TestDir::SRC_FAIL_ROOT)] =  "SRC_FAIL_ROOT";
-		m_keys[to_index(TestDir::SRC_PASS_ROOT)] =  "SRC_PASS_ROOT";
+		m_keys[to_index(TestDir::PROJECT_ROOT)]   = "PROJECT_ROOT";
+		m_keys[to_index(TestDir::SRC_FAIL_ROOT)]  = "SRC_FAIL_ROOT";
+		m_keys[to_index(TestDir::SRC_PASS_ROOT)]  = "SRC_PASS_ROOT";
 		m_keys[to_index(TestDir::DATA_FAIL_ROOT)] = "DATA_FAIL_ROOT";
 		m_keys[to_index(TestDir::DATA_PASS_ROOT)] = "DATA_PASS_ROOT";
-		m_keys[to_index(TestDir::MODEL_ROOT)] =     "MODEL_ROOT";
-		m_keys[to_index(TestDir::ALPHA_PATH)] =     "ALPHA_PATH";		
-		m_keys[to_index(TestDir::BORDER_PATH)] =    "BORDER_PATH";
+		m_keys[to_index(TestDir::MODEL_ROOT)]     = "MODEL_ROOT";
+		m_keys[to_index(TestDir::ALPHA_PATH)]     = "ALPHA_PATH";		
+		m_keys[to_index(TestDir::BORDER_PATH)]    = "BORDER_PATH";
 
 		m_config = cr::read_config(TEST_DIR_CONFIG_PATH, m_keys);
 	}
