6fba044e53be0b5323357ca2e516adc5a775ea96
adam-lafontaine <adam.lafontaine@gmail.com>
2021-03-19 10:44:42 -0400

    replace boost gil

--- ImageFactory/src/image_factory.cpp
+++ ImageFactory/src/image_factory.cpp
@@ -4,7 +4,7 @@ Copyright (c) 2021 Adam Lafontaine
 
 */
 
-#include "../../utils/libimage.hpp"
+#include "../../utils/libimage/libimage_fs.hpp"
 #include "image_factory.hpp"
 
 #include <random>
@@ -21,64 +21,36 @@ Copyright (c) 2021 Adam Lafontaine
 namespace fs = std::filesystem;
 namespace img = libimage;
 
-using index_t = img::index_t;
+using pixel_range_t = img::pixel_range_t;
+using range_list_t = std::vector<img::pixel_range_t>;
+using color = img::pixel_t;
 
-using color = img::rgb_t;
+const auto BLACK = img::to_pixel(0);
 
-typedef struct
-{
-	img::index_t x_begin;
-	img::index_t x_end;
-	img::index_t y_begin;
-	img::index_t y_end;
-} pixel_range_t;
+const auto GREY = img::to_pixel(195);
 
-using range_list_t = std::vector<pixel_range_t>;
+const auto ORANGE = img::to_pixel(255, 128, 0);
+const auto ORANGE_1 = img::to_pixel(200, 100, 0);
 
-constexpr img::bits32 BLACK32 = 0;
-constexpr img::bits32 RED32 = img::to_bits32(255, 0, 0);
-constexpr img::bits32 GREEN32 = img::to_bits32(0, 255, 0);
-constexpr img::bits32 BLUE32 = img::to_bits32(0, 0, 255);
-constexpr img::bits32 WHITE32 = img::to_bits32(255, 255, 255);
+const auto PURPLE = img::to_pixel(163, 73, 164);
+const auto PURPLE_1 = img::to_pixel(150, 73, 180);
 
-constexpr img::bits32 SRC_EDGE_COLOR = BLACK32;
-constexpr img::bits32 SRC_LETTER_COLOR = BLUE32;
-constexpr img::bits32 SRC_SURFACE_COLOR = GREEN32;
-constexpr img::bits32 SRC_BACKGROUND_COLOR = WHITE32;
+const auto YELLOW = img::to_pixel(170, 170, 0);
+const auto YELLOW_1 = img::to_pixel(150, 150, 0);
 
-constexpr color GREY = { 195, 195, 195 };
-constexpr color BLACK = { 0, 0, 0 };
-constexpr color WHITE = { 255, 255, 255 };
+const auto RED = img::to_pixel(180, 0, 0);
+const auto RED_1 = img::to_pixel(90, 0, 0);
 
-constexpr color ORANGE = { 255, 128, 0 };
-constexpr color ORANGE_1 = { 200, 100, 0 };
+const auto BLUE = img::to_pixel(0, 64, 128);
+const auto BLUE_1 = img::to_pixel(0, 30, 170);
 
-constexpr color PURPLE = { 163, 73, 164 };
-constexpr color PURPLE_1 = { 150, 73, 180 };
+const auto GREEN = img::to_pixel(0, 128, 0);
+const auto GREEN_1 = img::to_pixel(10, 220, 10);
 
-constexpr color YELLOW = { 170, 170, 0 };
-constexpr color YELLOW_1 = { 150, 150, 0 };
+const auto BLACK32 = BLACK.value;
 
-constexpr color RED = { 180, 0, 0 };
-constexpr color RED_1 = { 90, 0, 0 };
-
-constexpr color BLUE = { 0, 64, 128 };
-constexpr color BLUE_1 = { 0, 30, 170 };
-
-constexpr color GREEN = { 0, 128, 0 };
-constexpr color GREEN_1 = { 10, 220, 10 };
-
-
-constexpr std::array<color, 6> LETTER_COLORS = { ORANGE, PURPLE, YELLOW, ORANGE_1, PURPLE_1, YELLOW_1 };
-constexpr std::array<color, 6> SURFACE_COLRS = { RED, BLUE, GREEN, RED_1, BLUE_1, GREEN_1 };
-
-
-static img::bits32 to_bits32(img::ref_t const& p)
-{
-	const auto rgb = img::to_rgb(p);
-
-	return img::to_bits32(rgb);
-}
+const std::vector<color> LETTER_COLORS = { ORANGE, PURPLE, YELLOW, ORANGE_1, PURPLE_1, YELLOW_1 };
+const std::vector<color> SURFACE_COLRS = { RED, BLUE, GREEN, RED_1, BLUE_1, GREEN_1 };
 
 
 
@@ -87,18 +59,18 @@ static range_list_t get_letter_ranges(img::view_t const& img_v)
 {
 	range_list_t letter_ranges;
 
-	const auto width = img_v.width();
-	const auto height = img_v.height();
+	const auto width = img_v.width;
+	const auto height = img_v.height;
 
 	bool found = false;
-	const auto is_black_pred = [](auto const& rgb) { return img::to_bits32(rgb) == BLACK32; };
+	const auto is_black_pred = [](color const& p) { return p.value == BLACK32; };
 
-	index_t x_begin = 0;
+	u32 x_begin = 0;
 
 	// find horizontal positions of each letter
-	for (index_t x = 0; x < width; ++x)
+	for (u32 x = 0; x < width; ++x)
 	{
-		auto column = img::to_rgb_column(img_v, x);
+		auto column = img::column_view(img_v, x);
 		bool has_black = std::any_of(column.begin(), column.end(), is_black_pred);
 
 		if (!found && has_black) // found letter
@@ -124,9 +96,9 @@ static range_list_t get_letter_ranges(img::view_t const& img_v)
 	found = false;
 	for (auto& range : letter_ranges)
 	{
-		for (index_t y = range.y_begin; y < range.y_end; ++y)
+		for (u32 y = range.y_begin; y < range.y_end; ++y)
 		{
-			auto row = img::to_rgb_row(img_v, range.x_begin, range.x_end, y);
+			auto row = img::row_view(img_v, range.x_begin, range.x_end, y);
 			bool has_black = std::any_of(row.begin(), row.end(), is_black_pred);
 
 			if (!found && has_black)
@@ -151,14 +123,14 @@ static range_list_t get_letter_ranges(img::view_t const& img_v)
 // get pixel positions to center a smaller view inside a larger one
 static pixel_range_t get_centered(img::view_t const& large, img::view_t const& small)
 {
-	auto center_x = large.width() / 2;
-	auto center_y = large.height() / 2;
+	auto center_x = large.width / 2;
+	auto center_y = large.height / 2;
 
 	pixel_range_t range;
-	range.x_begin = center_x - small.width() / 2;
-	range.x_end = range.x_begin + small.width();
-	range.y_begin = center_y - small.height() / 2;
-	range.y_end = range.y_begin + small.height();
+	range.x_begin = center_x - small.width / 2;
+	range.x_end = range.x_begin + small.width;
+	range.y_begin = center_y - small.height / 2;
+	range.y_end = range.y_begin + small.height;
 
 	return range;
 }
@@ -179,7 +151,7 @@ static void assemble_blocks(
 	// predicate for determining if an x,y position should have the letter color
 	const auto is_letter = [&](auto x, auto y)
 	{
-		// is is inside the dimenstions of the view?
+		// is it inside the dimensions of the view?
 		if (x < letter_range.x_begin || x >= letter_range.x_end || y < letter_range.y_begin || y >= letter_range.y_end)
 			return false;
 
@@ -187,7 +159,7 @@ static void assemble_blocks(
 		auto y_pos = y - letter_range.y_begin;
 
 		// is the pixel black?
-		return to_bits32(*(letter_v.xy_at(x_pos, y_pos))) == BLACK32;
+		return (*(letter_v.xy_at(x_pos, y_pos))).value == BLACK32;
 	};
 
 	// determine where to place the border
@@ -202,7 +174,7 @@ static void assemble_blocks(
 		auto x_pos = x - border_range.x_begin;
 		auto y_pos = y - border_range.y_begin;
 
-		return to_bits32(*(border_v.xy_at(x_pos, y_pos))) == BLACK32;
+		return (*(border_v.xy_at(x_pos, y_pos))).value == BLACK32;
 	};
 
 	//predicate for determinining if an x,y position should have the surface color
@@ -214,30 +186,28 @@ static void assemble_blocks(
 		auto x_pos = x - border_range.x_begin;
 		auto y_pos = y - border_range.y_begin;
 
-		return to_bits32(*(border_v.xy_at(x_pos, y_pos))) != BLACK32;
+		return (*(border_v.xy_at(x_pos, y_pos))).value != BLACK32;
 	};
 
 	// iterate over every pixel and set its color
-	for (index_t y = 0; y < dst_list[0].height(); ++y)
+
+	for (auto& view : dst_list)
 	{
-		img::pixel_ptr_list_t dst_it_list;
-		dst_it_list.reserve(dst_list.size());
-
-		for(size_t i = 0; i < dst_list.size(); ++i)
-			dst_it_list.push_back(dst_list[i].row_begin(y));
-
-		for (index_t x = 0; x < dst_list[0].width(); ++x)
+		for (u32 y = 0; y < view.height; ++y)
 		{
-			auto c = GREY;
-			if (is_letter(x, y))
-				c = letter_c;
-			else if (is_surface(x, y))
-				c = surface_c;
-			else if (is_border(x, y))
-				c = BLACK;
+			auto ptr = view.row_begin(y);
+			for (u32 x = 0; x < view.width; ++x)
+			{
+				auto c = GREY;
+				if (is_letter(x, y))
+					c = letter_c;
+				else if (is_surface(x, y))
+					c = surface_c;
+				else if (is_border(x, y))
+					c = BLACK;
 
-			for(auto& dst_it : dst_it_list)
-				dst_it[x] = to_pixel(c);			
+				ptr[x] = c;
+			}
 		}
 	}
 }
@@ -246,12 +216,12 @@ static void assemble_blocks(
 // draw a line in an image
 static void draw_line(img::view_t const& view, pixel_range_t const& range)
 {
-	for (index_t y = range.y_begin; y < range.y_end; ++y)
+	for (u32 y = range.y_begin; y < range.y_end; ++y)
 	{
 		auto it = view.row_begin(y);
-		for (index_t x = range.x_begin; x < range.x_end; ++x)
+		for (u32 x = range.x_begin; x < range.x_end; ++x)
 		{
-			it[x] = to_pixel(BLACK);
+			it[x] = BLACK;
 		}
 	}
 }
@@ -266,9 +236,9 @@ static void add_defects(img::view_t const& view)
 
 	typedef struct
 	{
-		index_t y_pos;
-		index_t begin;
-		index_t end;
+		u32 y_pos;
+		u32 begin;
+		u32 end;
 	}  x_range;
 
 	// get number of defects
@@ -277,9 +247,9 @@ static void add_defects(img::view_t const& view)
 	std::uniform_int_distribution<unsigned> uni_lines(min_lines, max_lines);
 	auto num_lines = uni_lines(eng);
 
-	std::uniform_int_distribution<index_t> y_dist(100, view.height() - 100);
-	std::uniform_int_distribution<index_t> x_dist(100, view.width() - 200);
-	std::uniform_int_distribution<index_t> len_dist(10, 100);	
+	std::uniform_int_distribution<u32> y_dist(100, view.height - 100);
+	std::uniform_int_distribution<u32> x_dist(100, view.width - 200);
+	std::uniform_int_distribution<u32> len_dist(10, 100);	
 
 	for (size_t i = 0; i < num_lines; ++i)
 	{
@@ -297,29 +267,29 @@ static void add_defects(img::view_t const& view)
 // builds all of the images 
 void build_images(const char* alpha_path, const char* border_path, const char* pass_dir, const char* fail_dir)
 {
-	auto alpha_img = img::read_image_from_file(alpha_path);
+	img::image_t alpha_img;
+	img::read_image_from_file(alpha_path, alpha_img);
 	auto alpha_v = img::make_view(alpha_img);
 
 	auto ranges = get_letter_ranges(alpha_v);
-
 	std::vector<img::view_t> letters;
-	letters.reserve(ranges.size());
 
-	for (auto const& range : ranges)
-	{
-		auto width = range.x_end - range.x_begin;
-		auto height = range.y_end - range.y_begin;
-		auto dst_v = img::view_t(width, height, alpha_v.xy_at(range.x_begin, range.y_begin));
+	std::transform(ranges.begin(), ranges.end(), std::back_inserter(letters), 
+		[&](auto const& r) { return img::sub_view(alpha_v, r); });
 
-		letters.push_back(dst_v);
-	}
-
-	auto border_img = img::read_image_from_file(border_path);
+	img::image_t border_img;
+	img::read_image_from_file(border_path, border_img);
 	auto border_v = img::make_view(border_img);
 
 	// decide the image dimensions
-	const auto dst_w = border_v.width() + 96;
-	const auto dst_h = border_v.height() + 96;
+	const auto dst_w = border_v.width + 96;
+	const auto dst_h = border_v.height + 96;
+
+	img::image_t pass_img;
+	img::image_t fail_img;
+
+	img::make_image(pass_img, dst_w, dst_h);
+	img::make_image(fail_img, dst_w, dst_h);
 
 	const auto num_images = letters.size() * SURFACE_COLRS.size() * LETTER_COLORS.size();
 	const auto idx_len = std::to_string(num_images).length();
@@ -343,8 +313,7 @@ void build_images(const char* alpha_path, const char* border_path, const char* p
 				const auto pass_path = fs::path(pass_dir) / file_name;
 				const auto fail_path = fs::path(fail_dir) / file_name;
 				
-				img::image_t pass_img(dst_w, dst_h);
-				img::image_t fail_img(dst_w, dst_h);
+				
 
 				std::vector<img::view_t> view_list { img::make_view(pass_img), img::make_view(fail_img) };
 
@@ -353,8 +322,8 @@ void build_images(const char* alpha_path, const char* border_path, const char* p
 				// modify fail images
 				add_defects(view_list[fail_idx]);
 
-				img::write_image_view(pass_path.c_str(), view_list[pass_idx]);
-				img::write_image_view(fail_path.c_str(), view_list[fail_idx]);
+				img::write_view(view_list[pass_idx], pass_path);
+				img::write_view(view_list[fail_idx], fail_path);
 			}
 		}
 
