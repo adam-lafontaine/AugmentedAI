ccf4d9d7e3a5eb18b6c5d1e2bed7afd3ed00231a
adam-lafontaine <adam.lafontaine@gmail.com>
2021-03-26 09:35:32 -0400

    image factory

--- LinuxCompile/Rpi/image_factory.sh
+++ LinuxCompile/Rpi/image_factory.sh
@@ -5,9 +5,10 @@ timestamp() {
   date +"%T"
 }
 
-incl_dirs="-I/usr/local/boost_1_73_0"
+incl_dirs="" #"-I/usr/local/boost_1_73_0"
 lib_dirs="" #"-L/..."
-links="-lstdc++fs -lpng"
+#links="-lstdc++fs -lpng"
+links="-lstdc++fs"
 
 log_file="compile.log"
 
@@ -17,13 +18,16 @@ std="-std=c++17"
 utils="../../utils"
 ImageFactory="../../ImageFactory/src"
 
+#utils
 config_reader="$utils/config_reader.cpp"
 dirhelper="$utils/dirhelper.cpp"
+libimage="$utils/libimage/libimage.cpp"
+utils_cpp="$config_reader $dirhelper $libimage"
 
 image_factory="$ImageFactory/image_factory.cpp"
-main_cpp="$ImageFactory/source_main.cpp"
+main_cpp="$ImageFactory/image_factory_main.cpp"
 
-cpp_files="$main_cpp $image_factory $config_reader $dirhelper"
+cpp_files="$main_cpp $image_factory $utils_cpp"
 
 exe="ImageFactory"
 
--- LinuxCompile/Ubuntu/image_factory.sh
+++ LinuxCompile/Ubuntu/image_factory.sh
@@ -13,17 +13,21 @@ std="-std=c++17"
 utils="../../utils"
 ImageFactory="../../ImageFactory/src"
 
+#utils
 config_reader="$utils/config_reader.cpp"
 dirhelper="$utils/dirhelper.cpp"
+libimage="$utils/libimage/libimage.cpp"
+utils_cpp="$config_reader $dirhelper $libimage"
 
 image_factory="$ImageFactory/image_factory.cpp"
-main_cpp="$ImageFactory/source_main.cpp"
+main_cpp="$ImageFactory/image_factory_main.cpp"
 
-cpp_files="$main_cpp $image_factory $config_reader $dirhelper"
+cpp_files="$main_cpp $image_factory $utils_cpp"
 
 exe="ImageFactory"
 
 # complile and write to file
 timestamp > $log_file
-g++-10 -o $exe $flags $cpp_files $std `pkg-config --cflags --libs vcpkg_all` &>> $log_file
+#g++-10 -o $exe $flags $cpp_files $std `pkg-config --cflags --libs vcpkg_all` &>> $log_file
+g++-10 -o $exe $flags $cpp_files $std &>> $log_file
 timestamp >> $log_file
